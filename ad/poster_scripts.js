b5cbf7a13ee4b168471173154ab01af087b0992b
Verfaieli <sandra.tolkacz@gmail.com>
2017-05-16 19:17:16 +0200

    Poprawienie ogłoszeń
    Chyba nie muszę dawać checkboxów, bo z tego co napisał kolega na grupie,
    do ogłoszenia można dodać tylko jedną kategorię. A więc lista wystarczy.
    Chyba. Mam nadzieję.

---
+++ ad/poster_scripts.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function poster_hours_trigger() {
+    document.getElementById("poster_hours_text_input").disabled = false;
+    
+}
